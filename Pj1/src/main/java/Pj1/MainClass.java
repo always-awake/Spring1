a8bb29a2a4a0b8a3c815e5b5397ffbfd54a977a4
leemirim <leemirim9654@gmail.com>
2019-05-31 15:54:57 +0900

    applicationContext.xml 파일을 생성해 컨테이너 생성해보기

---
+++ Pj1/src/main/java/Pj1/MainClass.java
@@ -0,0 +1,20 @@
+package Pj1;
+
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+//        TransportationWalk transportationWalk = new TransportationWalk();
+//        transportationWalk.move();
+
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+
+        TransportationWalk transportationWalk = ctx.getBean("Pj1Walk", TransportationWalk.class);
+        transportationWalk.move();
+
+        ctx.close();
+    }
+}
